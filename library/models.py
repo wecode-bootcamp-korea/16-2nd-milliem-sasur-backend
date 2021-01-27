f056eabc97ff85328e3f258d555e87576a2eb9ec
donq <donqlee88@gmail.com>
2021-01-27 17:22:50 +0900

    모델링
    모델링_1차 수정
    
    모델링_2차 수정

---
+++ library/models.py
@@ -0,0 +1,14 @@
+from django.db     import models
+
+class Library(models.Model):
+    name = models.CharField(max_length=45, default='나의 서재')
+
+    class Meta:
+        db_table = 'libraries'
+
+class Shelf(models.Model):
+    name    = models.CharField(max_length=100)
+    library = models.ForeignKey(Library, on_delete=models.CASCADE, related_name='shelves')
+
+    class Meta:
+        db_table = 'shelves'
--- milliem/settings.py
+++ milliem/settings.py
@@ -39,6 +39,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'corsheaders',
+    'users',
+    'book',
+    'library'
 ]
 
 MIDDLEWARE = [
